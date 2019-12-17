cf69378a5f9e1eb8a84120324e1d3aa6b28c8383
hugumoraes <50657099+hugumoraes@users.noreply.github.com>
2019-12-17 04:50:24 -0300

    Revert "a"
    This reverts commit c3a547f875c63219530531cb2c6d510d682cf79c.

--- server-data/resources/[esx]/esx_whitelistEnhanced/config.lua
+++ server-data/resources/[esx]/esx_whitelistEnhanced/config.lua
@@ -2,7 +2,7 @@ Config = {}
 
 Config.Locale = 'en'
 
-Config.EnableAntiSpam = true
+Config.EnableAntiSpam = false
 Config.WaitingTime = 5
 Config.CommunityLink = 'https://discord.gg/AD25MMS'
 Config.PlayersToStartRocade = 32
