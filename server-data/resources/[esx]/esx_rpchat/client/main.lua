94779166db1788db1533760d42bcb9ac243b4349
Jean Myamoto <jeantherox@gmail.com>
2019-12-17 19:18:14 -0300

    chat roles 17/12

--- server-data/resources/[esx]/esx_rpchat/client/main.lua
+++ server-data/resources/[esx]/esx_rpchat/client/main.lua
@@ -14,15 +14,21 @@ AddEventHandler('esx_rpchat:sendProximityMessage', function(playerId, title, mes
 end)
 
 Citizen.CreateThread(function()
-	TriggerEvent('chat:addSuggestion', '/twt',  _U('twt_help'),  { { name = _U('generic_argument_name'), help = _U('generic_argument_help') } } )
-	TriggerEvent('chat:addSuggestion', '/me',   _U('me_help'),   { { name = _U('generic_argument_name'), help = _U('generic_argument_help') } } )
-	TriggerEvent('chat:addSuggestion', '/do',   _U('do_help'),   { { name = _U('generic_argument_name'), help = _U('generic_argument_help') } } )
+	TriggerEvent('chat:addSuggestion', '/tweet',  _U('twt_help'),  { { name = _U('generic_argument_name'), help = _U('generic_argument_help') } } )
+	TriggerEvent('chat:addSuggestion', '/ilegal',   _U('ilegal_help'),   { { name = _U('generic_argument_name'), help = _U('generic_argument_help') } } )
+	TriggerEvent('chat:addSuggestion', '/192',   _U('192_help'),   { { name = _U('generic_argument_name'), help = _U('generic_argument_help') } } )
+	TriggerEvent('chat:addSuggestion', '/190',   _U('190_help'),   { { name = _U('generic_argument_name'), help = _U('generic_argument_help') } } )
+	TriggerEvent('chat:addSuggestion', '/olx',   _U('olx_help'),   { { name = _U('generic_argument_name'), help = _U('generic_argument_help') } } )
+	TriggerEvent('chat:addSuggestion', '/forarp',   _U('forarp_help'),   { { name = _U('generic_argument_name'), help = _U('generic_argument_help') } } )
 end)
 
 AddEventHandler('onResourceStop', function(resource)
 	if resource == GetCurrentResourceName() then
-		TriggerEvent('chat:removeSuggestion', '/twt')
-		TriggerEvent('chat:removeSuggestion', '/me')
-		TriggerEvent('chat:removeSuggestion', '/do')
+		TriggerEvent('chat:removeSuggestion', '/tweet')
+		TriggerEvent('chat:removeSuggestion', '/ilegal')
+		TriggerEvent('chat:removeSuggestion', '/192')
+		TriggerEvent('chat:removeSuggestion', '/190')
+		TriggerEvent('chat:removeSuggestion', '/olx')
+		TriggerEvent('chat:removeSuggestion', '/forarp')
 	end
 end)
