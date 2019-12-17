45520d63b59bb5ae67c3ca54e006bbf1d8da2d28
Jean Myamoto <jeantherox@gmail.com>
2019-12-17 04:00:39 -0300

    Corringindo frases whitelist 17/12

--- server-data/resources/[esx]/esx_whitelistEnhanced/locales/en.lua
+++ server-data/resources/[esx]/esx_whitelistEnhanced/locales/en.lua
@@ -1,14 +1,14 @@
 Locales['en'] = {
-  ['not_in_whitelist'] = 'This server is using a whitelist. To talk about it, join us here : %s',
+  ['not_in_whitelist'] = 'Este servidor está usando Whitelist, abra a steam e faça a whitelist no discord: https://discord.gg/AD25MMS  %s',
   ['missing_steam_id'] = 'Unable to find your steamID',
   ['banned_from_server'] = 'You are banned from this server',
-  ['waiting_queue_message'] = 'You are in the waiting queue (%i/%i)',
+  ['waiting_queue_message'] = 'Você está na fila na posição: (%i/%i)',
   ['waiting_free_priority_slots'] = 'Waiting for a priority slot (%i ppriority slots, estimated waiting %im %is)',
 
   ['log_trying_to_connect'] = '%s [%s] is trying to connect',
   ['log_missing_steam_id'] = 'Player %s doesn\'t have a steamID',
   ['log_banned_from_server'] = '%s [%s] is banned : \'%s\'',
-  ['log_not_in_whitelist'] = '%s [%s] is not in whitelist',
+  ['log_not_in_whitelist'] = '%s [%s] não está na whitelist',
   ['log_already_in_priority_queue'] = '%s [%s] is already in priority queue',
   ['log_added_to_priority_queue'] = '%s [%s] was added to priority queue',
   ['log_removed_from_priority_queue'] = '%s [%s] was removed from priority queue',
