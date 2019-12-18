5ad09665596fe2779d09f7091ca0247572d4870b
taeyeonsooyoung <58788941+taeyeonsooyoung@users.noreply.github.com>
2019-12-17 23:02:29 -0300

    update garagem

--- server-data/resources/[esx]/esx_drpgarage/server/server.lua
+++ server-data/resources/[esx]/esx_drpgarage/server/server.lua
@@ -115,9 +115,9 @@ end)
 -- Find player vehicles
 function getPlayerVehicles(identifier)
 	local vehicles = {}
-	local data = MySQL.Sync.fetchAll('SELECT * FROM owned_vehicles WHERE owner=@identifier AND type = @type',{['@identifier'] = identifier}){	
+	local data = MySQL.Sync.fetchAll('SELECT * FROM owned_vehicles WHERE owner=@identifier AND type = @type',{['@identifier'] = identifier})
 	['@type']   = 'Esportivo'
-	} for _,v in pairs(data) do
+	 for _,v in pairs(data) do
 		local vehicle = json.decode(v.vehicle)
 		table.insert(vehicles, {id = v.id, plate = v.plate})
 	end
