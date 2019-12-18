077764d3f18cf95cfd7668553d61e0a28d524f11
Jean Myamoto <jeantherox@gmail.com>
2019-12-17 21:41:20 -0300

    Revert "tirar nome ilegal, chat invisivel"
    This reverts commit d3a7be1010aa4e82a67a91f618ba6fc76864ab4a.

--- server-data/resources/[esx]/esx_rpchat/server/main.lua
+++ server-data/resources/[esx]/esx_rpchat/server/main.lua
@@ -23,7 +23,7 @@ RegisterCommand('tweet', function(source, args, rawCommand)
 	if Config.EnableESXIdentity then name = GetCharacterName(source) end
 
 	TriggerClientEvent('chat:addMessage', -1, { args = { _U('tweet_prefix', name), message }, color = { 0, 153, 204 } })
-	print(('%s: %s'):format(name, args))
+	--print(('%s: %s'):format(name, args))
 end, false)
 
 RegisterCommand('ilegal', function(source, args, rawCommand)
@@ -36,8 +36,8 @@ RegisterCommand('ilegal', function(source, args, rawCommand)
 	local name = GetPlayerName(source)
 	if Config.EnableESXIdentity then name = GetCharacterName(source) end
 
-	TriggerClientEvent('chat:addMessage', -1, { args = { _U('ilegal_prefix'), message }, color = { 41, 53, 59 } })
-	print((': %s'):format(args))
+	TriggerClientEvent('chat:addMessage', -1, { args = { _U('ilegal_prefix', name), message }, color = { 41, 53, 59 } })
+	--print(('%s: %s'):format(name, args))
 end, false)
 
 RegisterCommand('192', function(source, args, rawCommand)
@@ -51,7 +51,7 @@ RegisterCommand('192', function(source, args, rawCommand)
 	if Config.EnableESXIdentity then name = GetCharacterName(source) end
 
 	TriggerClientEvent('chat:addMessage', -1, { args = { _U('192_prefix', name), message }, color = { 246, 3, 59 } })
-	print(('%s: %s'):format(name, args))
+	--print(('%s: %s'):format(name, args))
 end, false)
 
 RegisterCommand('190', function(source, args, rawCommand)
@@ -65,7 +65,7 @@ RegisterCommand('190', function(source, args, rawCommand)
 	if Config.EnableESXIdentity then name = GetCharacterName(source) end
 
 	TriggerClientEvent('chat:addMessage', -1, { args = { _U('190_prefix', name), message }, color = { 130, 130, 255 } })
-	print(('%s: %s'):format(name, args))
+	--print(('%s: %s'):format(name, args))
 end, false)
 
 RegisterCommand('olx', function(source, args, rawCommand)
@@ -79,7 +79,7 @@ RegisterCommand('olx', function(source, args, rawCommand)
 	if Config.EnableESXIdentity then name = GetCharacterName(source) end
 
 	TriggerClientEvent('chat:addMessage', -1, { args = { _U('olx_prefix', name), message }, color = { 239, 239, 56 } })
-	print(('%s: %s'):format(name, args))
+	--print(('%s: %s'):format(name, args))
 end, false)
 
 RegisterCommand('forarp', function(source, args, rawCommand)
@@ -93,7 +93,7 @@ RegisterCommand('forarp', function(source, args, rawCommand)
 	if Config.EnableESXIdentity then name = GetCharacterName(source) end
 
 	TriggerClientEvent('chat:addMessage', -1, { args = { _U('forarp_prefix', name), message }, color = { 239, 239, 56 } })
-	print(('%s: %s'):format(name, args))
+	--print(('%s: %s'):format(name, args))
 end, false)
 
 function GetCharacterName(source)
