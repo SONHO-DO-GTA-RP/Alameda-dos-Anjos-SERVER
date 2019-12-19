d489fb6b20088301bf5344a8e9dd4ace918e8dc6
Jean Myamoto <jeantherox@gmail.com>
2019-12-19 02:51:31 -0300

    todas as armas adicionadas 19/12

--- server-data/resources/[esx]/esx_traficodearmas/server/main.lua
+++ server-data/resources/[esx]/esx_traficodearmas/server/main.lua
@@ -104,3 +104,36 @@ ESX.RegisterUsableItem('PISTOL', function(source)
 
 	TriggerClientEvent('esx:showNotification', source, _U('used_weapbox'))
 end)
+
+ESX.RegisterUsableItem('SMG', function(source)
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+	xPlayer.removeInventoryItem('SMG', 1)
+	xPlayer.addWeapon ('WEAPON_SMG', 120)
+
+	cb(true)
+
+	TriggerClientEvent('esx:showNotification', source, _U('used_weapbox'))
+end)
+
+ESX.RegisterUsableItem('SHOTGUN', function(source)
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+	xPlayer.removeInventoryItem('SHOTGUN', 1)
+	xPlayer.addWeapon ('WEAPON_SHOTGUN', 48)
+
+	cb(true)
+
+	TriggerClientEvent('esx:showNotification', source, _U('used_weapbox'))
+end)
+
+ESX.RegisterUsableItem('AK', function(source)
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+	xPlayer.removeInventoryItem('AK', 1)
+	xPlayer.addWeapon ('WEAPON_ASSAULTRIFLE', 200)
+
+	cb(true)
+
+	TriggerClientEvent('esx:showNotification', source, _U('used_weapbox'))
+end)
