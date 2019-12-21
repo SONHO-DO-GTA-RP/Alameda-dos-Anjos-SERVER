c28a2325ba9ee861554cb179068027c451af5c06
hugumoraes <50657099+hugumoraes@users.noreply.github.com>
2019-12-20 22:36:02 -0300

    GYM

---
+++ server-data/resources/[esx]/[esx_jobs]/esx_gym/esx_gym.sql
@@ -0,0 +1,6 @@
+INSERT INTO `items` (name, label) VALUES 
+	('gym_membership', 'Plano da academia'),
+	('powerade', 'Gatorade'),
+	('sportlunch', 'Supercalórico'),
+	('protein_shake', 'Shake de proteína')
+;
