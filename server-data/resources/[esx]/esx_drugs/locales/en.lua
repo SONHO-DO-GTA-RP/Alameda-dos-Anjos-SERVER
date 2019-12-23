a2fc8eb2af1eff830ca552178287ebafb244ed8c
Jean Myamoto <jeantherox@gmail.com>
2019-12-23 17:53:25 -0300

    tradução e preço da maconha 21/12

--- server-data/resources/[esx]/esx_drugs/locales/en.lua
+++ server-data/resources/[esx]/esx_drugs/locales/en.lua
@@ -1,20 +1,20 @@
 Locales ['en'] = {
   -- weed
-  ['weed_pickupprompt'] = 'press ~INPUT_CONTEXT~ to harvest the ~g~Cannabis~s~ plant.',
-  ['weed_inventoryfull'] = 'you do not have any more inventory space for ~g~Cannabis~s~.',
-  ['weed_processprompt'] = 'press ~INPUT_CONTEXT~ to start ~g~Process Cannabis~s~.',
-  ['weed_processingstarted'] = 'processing ~g~Cannabis~s~ into ~g~Marijuana~s~...',
-  ['weed_processingfull'] = 'processing ~r~canceled~s~ due to full inventory!',
-  ['weed_processingenough'] = 'you must have ~b~3x~s~ ~g~Cannabis~s~ in order to process.',
-  ['weed_processed'] = 'you\'ve processed ~b~3x~s~ ~g~Cannabis~s~ to ~b~1x~s~ ~g~Marijuana~s~',
-  ['weed_processingtoofar'] = 'the processing has been ~r~canceled~s~ due to you abandoning the area.',
+  ['weed_pickupprompt'] = 'Aperte ~INPUT_CONTEXT~ para colher a planta de ~g~Maconha~s~.',
+  ['weed_inventoryfull'] = 'Você não tem espaço suficiente para pegar mais ~g~Cannabis~s~.',
+  ['weed_processprompt'] = 'Aperte ~INPUT_CONTEXT~ para começar o ~g~Process Cannabis~s~.',
+  ['weed_processingstarted'] = 'Processando ~g~Cannabis~s~ em ~g~Marijuana~s~...',
+  ['weed_processingfull'] = 'Processo ~r~canceled~s~ devido ao inventário estar cheio!',
+  ['weed_processingenough'] = 'você tem que ter no mínimo ~b~2x~s~ ~g~Cannabis~s~ para poder processar.',
+  ['weed_processed'] = 'você processou ~b~2x~s~ ~g~Cannabis~s~ em ~b~1x~s~ ~g~Marijuana~s~',
+  ['weed_processingtoofar'] = 'o processamento foi ~r~canceled~s~. Você abandonou a área.',
 
   -- drug dealer
-  ['dealer_prompt'] = 'press ~INPUT_CONTEXT~ to talk with the ~r~Drug Dealer~s~.',
-  ['dealer_title'] = 'drug Dealer',
+  ['dealer_prompt'] = 'Aperte ~INPUT_CONTEXT~ para falar com o ~r~Drug Dealer~s~.',
+  ['dealer_title'] = 'Traficante de Maconha',
   ['dealer_item'] = '$%s',
-  ['dealer_notenough'] = 'you don\'t have enough of that to sell!',
-  ['dealer_sold'] = 'you\'ve sold ~b~%sx~s~ ~y~%s~s~ for ~g~$%s~s~',
+  ['dealer_notenough'] = 'Você não tem o suficiente para vender!',
+  ['dealer_sold'] = 'Você vendeu ~b~%sx~s~ ~y~%s~s~ por ~g~$%s~s~',
 
   -- license
   ['license_title'] = 'you are required to own a processing license to process this product, it can be bought here.',
@@ -24,7 +24,7 @@ Locales ['en'] = {
   ['license_weed'] = 'weed Processing License',
 
   -- blips
-  ['blip_weedfield'] = 'weed Field',
-  ['blip_weedprocessing'] = 'weed Processing',
-  ['blip_drugdealer'] = 'drug Dealer',
+  ['blip_weedfield'] = 'Campo de Maconha',
+  ['blip_weedprocessing'] = 'Processamento de Maconha',
+  ['blip_drugdealer'] = 'Traficante de Maconha',
 }
