370d3bf40529c547108f0ff3343a07cc54f5bc76
Jean Myamoto <jeantherox@gmail.com>
2019-12-27 18:23:15 -0300

    lavagem de dinheiro publico

---
+++ server-data/resources/[esx]/esx_moneywash/locales/en.lua
@@ -0,0 +1,12 @@
+Locales['en'] = {
+	['you_have_washed'] 		= 'Você lavou ~r~$',
+	['dirty_money'] 			= ' Dinheiro Sujo.',
+	['you_have_received'] 		= ' ~s~Você recebeu um total de ~g~$',
+	['clean_money'] 			= ' Dinheiro Limpo',
+	['invalid_amount']			= 'Você entrou uma quantidade inválida',
+	['press_menu'] 				= 'aperte ~INPUT_CONTEXT~ para ~r~lavar seu dinheiro.~s~.',
+	['no'] 						= 'não',
+	['wash_money']				= 'Lavar Dinheiro',
+	['washed_menu']				= 'Lavagem de Dinheiro',
+	['wash_money_amount']		= 'Quantidade de dinheiro para lavar'
+}
---
+++ server-data/resources/[esx]/esx_teleports/config.lua
@@ -0,0 +1,38 @@
+Config                            = {}
+
+Config.Teleporters = {
+	['EclipseTowers'] = {
+		['Job'] = 'police',
+		['Enter'] = { 
+			['x'] = -777.03137207031, 
+			['y'] = 319.640625, 
+			['z'] = 84.717643432617,
+			['Information'] = '[E] Go Up',
+		},
+		['Exit'] = {
+			['x'] = -781.83642578125, 
+			['y'] = 325.7190246582, 
+			['z'] = 175.85869567871, 
+			['Information'] = '[E] Go Down' 
+		}
+	},
+
+	['Money Wash'] = {
+		['Job'] = 'none',
+		['Enter'] = { 
+		 ['x'] = 632.65, 
+		 ['y'] = -3014.78, 
+		 ['z'] = 7.34,
+		 ['Information'] = '[E] Entrar' 
+		},
+		['Exit'] = {
+		 ['x'] = 1137.93, 
+		 ['y'] = -3198.26, 
+		 ['z'] = -39.67, 
+		 ['Information'] = '[E] Sair'
+		}
+	}
+	--Next here
+}
+
+
